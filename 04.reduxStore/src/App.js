42ee46f5a9f76d084f906e40e9668a5312127e16
Areshka <oleg1990moskalenko@gmail.com>
2020-09-14 01:46:21 +0300

    react-router-dom

--- 04.reduxStore/src/App.js
+++ 04.reduxStore/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
 import store from './store/store';
 
-import Link from './components/Router/Link';
-import Route from './components/Router/Route';
-import Router from './components/Router/Router';
-import Redirect from './components/Router/Redirect';
+// import Link from './components/Router/Link';
+// import Route from './components/Router/Route';
+// import Router from './components/Router/Router';
+// import Redirect from './components/Router/Redirect';
 
-import Users from './components/Users';
 import Auth from './components/Auth';
+import Users from './components/Users';
+import Profile from './components/Profile';
 import './App.css';
 
 
@@ -27,11 +30,16 @@ function App() {
             <Link to="/">Home</Link>
             <Link to="/users">Users</Link>
             <Link to="/login">Login</Link>
+            <Link to="/profile">Profile</Link>
           </nav>
-          <Route path="/" component={<h1>Hello</h1>} />
-          <Route path="/users" component={<Users />} />
-          <Route path="/login" component={<Auth />} />
-          {/* <Redirect to="/" /> */}
+          <Switch>
+            <Route exact path="/" render={(props) => <h1>Hello</h1>} />
+            <Route path="/users" render={(props) => <Users {...props} name={'Vasya'} />} />
+            <Route path="/login" component={Auth} />
+            <Route path="/profile/:id" component={Profile} />
+            <Route path="*" render={(props) => <h1>404</h1>} />
+            {/* <Redirect to="/" /> */}
+          </Switch>
         </div>
       </Router>
     </Provider>
