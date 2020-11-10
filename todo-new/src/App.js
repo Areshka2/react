77bc5a7a6ac506cd4edfd41058c3be48411cfbd6
Oleg M <oleg.moskalenko@gmail.com>
2020-11-10 16:25:21 +0200

    todo new

---
+++ todo-new/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import './App.scss';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <div className="container">
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/about'} component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ todo-new/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="?#">
+        Application
+      </a>
+      <button
+        className="navbar-toggler d-lg-none"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavId"
+        aria-controls="collapsibleNavId"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavId">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <NavLink to="/" exact className="nav-link" activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" className="nav-link" activeClassName="active">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
